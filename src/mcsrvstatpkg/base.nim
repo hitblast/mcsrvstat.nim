8ccd518614a582eca98b1f1dd1c3d0f41fa44149
HitBlast <hitblastlive@gmail.com>
2023-04-09 22:34:17 +0600

    reduced a lot of boilerplate

--- src/mcsrvstatpkg/base.nim
+++ src/mcsrvstatpkg/base.nim
@@ -4,6 +4,7 @@ import std/[
     httpclient,
     json,
     options,
+    sequtils,
     strformat
 ]
 
@@ -20,16 +21,11 @@ type
         data*: Option[JsonNode]
 
     ServerDebugValues* = object
-        ping*: bool
-        query*: bool
-        srv*: bool
-        querymismatch*: bool
-        ipinsrv*: bool
-        cnameinsrv*: bool
-        animatedmotd*: bool
-        cachetime*: int
-        cacheexpire*: int
-        apiversion*: int
+        ping*, query*, srv*, querymismatch*, ipinsrv*, cnameinsrv*, animatedmotd*: bool
+        cachetime*, cacheexpire*, apiversion*: int
+
+    ServerMOTD* = object
+        raw*, clean*, html*: seq[string]
 
 
 # Custom exception objects for handling data-related errors.
@@ -65,43 +61,47 @@ proc retrieveData(self: Server, key: string): JsonNode =
         except KeyError:
             raise DataError.newException("Server is offline / the given server platform is invalid.")
 
+# Helper-procedure for other procs based on retrieveData() with optional string returns.
+proc retrieveOptionalStr(self: Server, key: string): Option[string] =
+    try:
+        return some(self.retrieveData(key).getStr())
+    except DataError:
+        return none(string)
+
 
 # Procedure for getting server status.
 proc online*(self: Server): bool =
-    let online = self.retrieveData("online")
-    return online.getBool()
+    return self.retrieveData("online").getBool()
 
 # Procedure for getting the IP address of a server.
 proc ip*(self: Server): string =
-    let ip = self.retrieveData("ip")
-    return ip.getStr()
+    return self.retrieveData("ip").getStr()
 
 # Procedure for getting the port of a server.
 proc port*(self: Server): int =
-    let port = self.retrieveData("port")
-    return port.getInt()
+    return self.retrieveData("port").getInt()
 
 # Procedure for getting the debug values of a server.
 proc debug*(self: Server): ServerDebugValues =
-    let data = self.retrieveData("debug")
-    let debug = ServerDebugValues(
-        ping: data["ping"].getBool(),
-        query: data["query"].getBool(),
-        srv: data["srv"].getBool(),
-        querymismatch: data["querymismatch"].getBool(),
-        ipinsrv: data["ipinsrv"].getBool(),
-        cnameinsrv: data["cnameinsrv"].getBool(),
-        animatedmotd: data["animatedmotd"].getBool(),
-        cachetime: data["cachetime"].getInt(),
-        cacheexpire: data["cacheexpire"].getInt(),
-        apiversion: data["apiversion"].getInt()
-    )
+    let
+        data = self.retrieveData("debug")
+        debug = ServerDebugValues(
+            ping: data["ping"].getBool(),
+            query: data["query"].getBool(),
+            srv: data["srv"].getBool(),
+            querymismatch: data["querymismatch"].getBool(),
+            ipinsrv: data["ipinsrv"].getBool(),
+            cnameinsrv: data["cnameinsrv"].getBool(),
+            animatedmotd: data["animatedmotd"].getBool(),
+            cachetime: data["cachetime"].getInt(),
+            cacheexpire: data["cacheexpire"].getInt(),
+            apiversion: data["apiversion"].getInt()
+        )
     return debug
 
 # Procedure for getting the version of a server.
 proc version*(self: Server): string =
-    let version = self.retrieveData("version")
-    return version.getStr()
+    return self.retrieveData("version").getStr()
 
 # Procedure for getting the protocol of a server.
 proc protocol*(self: Server): Option[int] =
@@ -113,40 +113,40 @@ proc protocol*(self: Server): Option[int] =
 
 # Procedure for getting the hostname of a server.
 proc hostname*(self: Server): Option[string] =
-    try:
-        let hostname = self.retrieveData("hostname")
-        return some(hostname.getStr())
-    except DataError:
-        return none(string)
+    return self.retrieveOptionalStr("hostname")
 
 # Procedure for getting the software of a server.
 proc software*(self: Server): Option[string] =
-    try:
-        let software = self.retrieveData("software")
-        return some(software.getStr())
-    except DataError:
-        return none(string)
+    return self.retrieveOptionalStr("software")
 
 # Procedure for getting the map of a server.
 proc map*(self: Server): Option[string] =
-    try:
-        let map = self.retrieveData("map")
-        return some(map.getStr())
-    except DataError:
-        return none(string)
+    return self.retrieveOptionalStr("map")
 
 # Procedure for getting the gamemode of a server.
 proc gamemode*(self: Server): Option[string] =
-    try:
-        let gamemode = self.retrieveData("gamemode")
-        return some(gamemode.getStr())
-    except DataError:
-        return none(string)
+    return self.retrieveOptionalStr("gamemode")
 
 # Procedure for getting the ID of a server.
 proc serverid*(self: Server): Option[string] =
+    return self.retrieveOptionalStr("serverid")
+
+# Procedure for getting the MOTD of a server.
+proc motd*(self: Server): Option[ServerMOTD] =
     try:
-        let serverid = self.retrieveData("serverid")
-        return some(serverid.getStr())
+        let
+            data = self.retrieveData("motd")
+            raw = map(toSeq(data["raw"]), proc(x: JsonNode): string = x.getStr())
+            clean = map(toSeq(data["clean"]), proc(x: JsonNode): string = x.getStr())
+            html = map(toSeq(data["html"]), proc(x: JsonNode): string = x.getStr())
+
+            motd = ServerMOTD(
+                raw: raw,
+                clean: clean,
+                html: html
+            )
+
+        return some(motd)
+
     except DataError:
-        return none(string)
+        return none(ServerMOTD)
